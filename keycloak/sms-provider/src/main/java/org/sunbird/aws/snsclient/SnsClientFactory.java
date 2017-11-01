fb1b0466630b620ab696dde5ad883be150624a08
kochhar <kochhar@users.noreply.github.com>
2017-11-01 14:26:01 +0530

    Added a TODO

--- keycloak/sms-provider/src/main/java/org/sunbird/aws/snsclient/SnsClientFactory.java
+++ keycloak/sms-provider/src/main/java/org/sunbird/aws/snsclient/SnsClientFactory.java
@@ -9,6 +9,7 @@ import com.amazonaws.services.sns.model.SetSMSAttributesRequest;
 /**
  * Created by nickpack on 09/08/2017.
  */
+// TODO(shriharshs): Create an interface for the SMS client factories
 public class SnsClientFactory {
     private static AmazonSNSClient snsClient = null;
 
