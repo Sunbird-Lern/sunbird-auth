f0c16d4c2e2b49bec113e40b18b07bba7bbb7324
Amol Ghatol <amol_g@techjoomla.com>
2018-03-29 14:28:32 +0530

    Issue #SB-2315 fix:Forgot Password-Starting letter of the message multiple users are associated with this phone. should be in uppercase

--- keycloak/sms-provider/src/main/java/org/sunbird/keycloak/utils/Constants.java
+++ keycloak/sms-provider/src/main/java/org/sunbird/keycloak/utils/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
   
   private Constants(){}
   
-  public static final String MULTIPLE_USER_ASSOCIATED_WITH_PHONE = "multiple users are associated with this phone.";
-  public static final String MULTIPLE_USER_ASSOCIATED_WITH_EMAIL = "multiple users are associated with this email.";
-  public static final String MULTIPLE_USER_ASSOCIATED_WITH_USERNAME = "multiple users are associated with this username.";
+  public static final String MULTIPLE_USER_ASSOCIATED_WITH_PHONE = "Multiple users are associated with this phone.";
+  public static final String MULTIPLE_USER_ASSOCIATED_WITH_EMAIL = "Multiple users are associated with this email.";
+  public static final String MULTIPLE_USER_ASSOCIATED_WITH_USERNAME = "Multiple users are associated with this username.";
 
 }
