75bf3a8e9b5e3f87b2b2fc7d6de21f7ade3794d4
AMIT KUMAR <amit.kumar@tarento.com>
2018-09-14 11:58:22 +0530

    Issue #SB-1420 feat:Expose a rest endpoint in keycloak to generate link for update password and verify email.

---
+++ keycloak/sms-provider/src/main/java/org/sunbird/keycloak/rest/RestResourceProvider.java
@@ -0,0 +1,196 @@
+package org.sunbird.keycloak.rest;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.ForbiddenException;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriBuilder;
+import org.keycloak.authentication.actiontoken.execactions.ExecuteActionsActionToken;
+import org.keycloak.common.util.Time;
+import org.keycloak.models.ClientModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.utils.KeycloakModelUtils;
+import org.keycloak.protocol.oidc.utils.RedirectUtils;
+import org.keycloak.services.ErrorResponse;
+import org.keycloak.services.managers.AppAuthManager;
+import org.keycloak.services.managers.AuthenticationManager.AuthResult;
+import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.services.resources.LoginActionsService;
+
+/**
+ * 
+ * @author Amit Kumar
+ *
+ */
+public class RestResourceProvider implements RealmResourceProvider {
+
+  private KeycloakSession session;
+  private AuthResult auth;
+  public RestResourceProvider(KeycloakSession session) {
+    this.session = session;
+    this.auth = new AppAuthManager().authenticateBearerToken(session, session.getContext().getRealm());
+    
+  }
+
+ /**
+   * create a link on which the user can click to reset their password or verify email based on action param value.
+   *
+   * @param redirectUri redirect uri
+   * @param clientId client id
+   * @param action action type
+   * @param userName userName
+   * @return
+   */
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response generateLink(Map<String, String> request) {
+    String redirectUri = request.get("redirectUri");
+    String clientId = request.get("clientId");
+    String action = request.get("action");
+    String userName = request.get("userName");
+    Integer lifespan = null;
+    try{
+      lifespan = Integer.parseInt(request.get("lifespan"));
+    }catch(Exception ex){
+      return ErrorResponse.error("Invalid lifespan value: " + lifespan, Status.BAD_REQUEST);
+    }
+    String authRequired = request.get("isAuthRequired");
+    if(null != authRequired){
+      Boolean isAuthRequired = null;
+      try{
+        isAuthRequired = Boolean.parseBoolean(authRequired);
+      }catch(Exception ex){
+        return ErrorResponse.error("Invalid isAuthRequired value: " + authRequired, Status.BAD_REQUEST);
+      }
+      if(isAuthRequired){
+        try
+        {
+          checkRealmAdmin();
+        }catch(NotAuthorizedException ex){
+          return ErrorResponse.error("Not Authorized.", Status.UNAUTHORIZED);
+        }catch(ForbiddenException ex){
+          return ErrorResponse.error("Does not have realm admin role." , Status.FORBIDDEN);
+        }
+      }
+    }
+    List<String> actions = new LinkedList<>();
+    if(UserModel.RequiredAction.UPDATE_PASSWORD.name().equalsIgnoreCase(action)){
+      actions.add(UserModel.RequiredAction.UPDATE_PASSWORD.name());
+    } else if(UserModel.RequiredAction.VERIFY_EMAIL.name().equalsIgnoreCase(action)){
+      actions.add(UserModel.RequiredAction.VERIFY_EMAIL.name());
+    } else{
+      return ErrorResponse.error("Invalid action type: " + action, Status.BAD_REQUEST);
+    }
+    return executeActions(redirectUri, clientId, lifespan, actions,userName);
+  }
+
+
+  /**
+   * create update password link
+   *
+   * create a link on which the user can click to perform a set of required actions. The
+   * redirectUri and clientId parameters are optional. If no redirect is given, then there will be
+   * no link back to click after actions have completed. Redirect uri must be a valid uri for the
+   * particular clientId.
+   *
+   * @param redirectUri Redirect uri
+   * @param clientId Client id
+   * @param lifespan Number of seconds after which the generated token expires
+   * @param actions required actions the user needs to complete
+   * @return
+   */
+  public Response executeActions(
+      String redirectUri,
+      String clientId,
+      Integer lifespan, List<String> actions, String userName ) {
+    UserModel user = KeycloakModelUtils.findUserByNameOrEmail(session, session.getContext().getRealm(),userName);
+    if (user == null) {
+      return ErrorResponse.error("Invalid userName : "+userName, Status.BAD_REQUEST);
+    }
+   
+    if (!user.isEnabled()) {
+      throw new WebApplicationException(
+          ErrorResponse.error("User is disabled", Status.BAD_REQUEST));
+    }
+
+    if (redirectUri != null && clientId == null) {
+      throw new WebApplicationException(
+          ErrorResponse.error("Client id missing", Status.BAD_REQUEST));
+    }
+
+    if (clientId == null) {
+      clientId = "account";
+    }
+
+    ClientModel client = session.getContext().getRealm().getClientByClientId(clientId);
+    if (client == null || !client.isEnabled()) {
+      throw new WebApplicationException(
+          ErrorResponse.error(clientId + " not enabled", Status.BAD_REQUEST));
+    }
+
+    String redirect;
+    if (redirectUri != null) {
+      redirect = RedirectUtils.verifyRedirectUri(session.getContext().getUri(), redirectUri,
+          session.getContext().getRealm(), client);
+      if (redirect == null) {
+        throw new WebApplicationException(
+            ErrorResponse.error("Invalid redirect uri.", Status.BAD_REQUEST));
+      }
+    }
+
+    if (lifespan == null) {
+      lifespan = 7200;
+    }
+    int expiration = Time.currentTime() + lifespan;
+    ExecuteActionsActionToken token =
+        new ExecuteActionsActionToken(user.getId(), expiration, actions, redirectUri, clientId);
+
+    try {
+      UriBuilder builder = LoginActionsService.actionTokenProcessor(session.getContext().getUri());
+      builder.queryParam("key",
+          token.serialize(session, session.getContext().getRealm(), session.getContext().getUri()));
+
+      String link = builder.build(session.getContext().getRealm().getName()).toString();
+      Map<String,Object> response = new HashMap<>();
+      response.put("link", link);
+      return Response.ok(response).build();
+    } catch (Exception e) {
+      return ErrorResponse.error("Failed to create link",
+          Status.INTERNAL_SERVER_ERROR);
+    }
+  }
+  
+  private void checkRealmAdmin() {
+    if (auth == null) {
+      System.out.println("auth is null;");
+        throw new NotAuthorizedException("Bearer");
+    } else if (auth.getToken().getRealmAccess() == null || !auth.getToken().getRealmAccess().isUserInRole("admin")) {
+      System.out.println("auth.getToken() "+auth.getToken());
+      System.out.println("auth.getToken().getRealmAccess() "+auth.getToken().getRealmAccess());
+      System.out.println("auth.getToken().getRealmAccess().isUserInRole(admin) "+auth.getToken().getRealmAccess().isUserInRole("admin"));
+      System.out.println("auth.getToken() is null or auth.getToken().getRealmAccess() is null or auth.getToken().getRealmAccess().isUserInRole(admin) is null");
+      throw new ForbiddenException("Does not have realm admin role");
+    }
+}
+
+  @Override
+  public Object getResource() {
+    return this;
+  }
+
+  @Override
+  public void close() {
+
+  }
+}
---
+++ keycloak/sms-provider/src/main/java/org/sunbird/keycloak/rest/RestResourceProviderFactory.java
@@ -0,0 +1,43 @@
+package org.sunbird.keycloak.rest;
+
+import org.keycloak.Config.Scope;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.services.resource.RealmResourceProviderFactory;
+
+/**
+ * 
+ * @author Amit Kumar
+ *
+ */
+public class RestResourceProviderFactory implements RealmResourceProviderFactory {
+
+  public static final String ID = "getlink";
+  
+  @Override
+  public String getId() {
+    return ID;
+  }
+  
+  @Override
+  public RealmResourceProvider create(KeycloakSession session) {
+    return new RestResourceProvider(session);
+  }
+
+  @Override
+  public void init(Scope config) {
+    
+  }
+
+  @Override
+  public void postInit(KeycloakSessionFactory factory) {
+    
+  }
+
+  @Override
+  public void close() {
+    
+  }
+
+}
