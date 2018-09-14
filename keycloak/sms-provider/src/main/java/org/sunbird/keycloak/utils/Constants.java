bcf92373f0279e4d7c923f2b6a32e3d085f85f14
AMIT KUMAR <amit.kumar@tarento.com>
2018-09-14 12:12:31 +0530

    Issue #SB-1420 feat:Expose a rest endpoint in keycloak to generate link for update password and verify email.

--- keycloak/sms-provider/src/main/java/org/sunbird/keycloak/utils/Constants.java
+++ keycloak/sms-provider/src/main/java/org/sunbird/keycloak/utils/Constants.java
@@ -7,5 +7,26 @@ public class Constants {
   public static final String MULTIPLE_USER_ASSOCIATED_WITH_PHONE = "Multiple users are associated with this phone.";
   public static final String MULTIPLE_USER_ASSOCIATED_WITH_EMAIL = "Multiple users are associated with this email.";
   public static final String MULTIPLE_USER_ASSOCIATED_WITH_USERNAME = "Multiple users are associated with this username.";
+  public static final String REDIRECT_URI = "redirectUri";
+  public static final String CLIENT_ID = "clientId";
+  public static final String ACTION = "action";
+  public static final String USERNAME = "userName";
+  public static final String LIFE_SPAN = "lifespan";
+  public static final String INVALID_LIFESPAN_VALUE = "Invalid lifespan value: ";
+  public static final String IS_AUTH_REQUIRED = "isAuthRequired";
+  public static final String INVALID_AUTH_REQRD_VALUE = "Invalid isAuthRequired value: ";
+  public static final String NOT_AUTHORIZED = "Not Authorized.";
+  public static final String DOES_NOT_HAVE_REALM_ADMIN_ROLE = "Does not have realm admin role.";
+  public static final String INVALID_ACTION = "Invalid action type: ";
+  public static final String INVALID_USERNAME = "Invalid userName : ";
+  public static final String USER_IS_DISABLED = "User is disabled.";
+  public static final String CLIENT_ID_MISSING = "Client id missing.";
+  public static final String NOT_ENABLED = " not enabled";
+  public static final String INVALID_REDIRECT_URI = "Invalid redirect uri.";
+  public static final String KEY = "key";
+  public static final String LINK = "link";
+  public static final String FAILED_TO_CREATE_LINK = "Failed to create link";
+  public static final String BEARER = "Bearer";
+  public static final String ACCOUNT= "account";
 
 }
