f16bdadc37f6dc9386b1996c234931223acd7587
bvinayakumar <36028332+bvinayakumar@users.noreply.github.com>
2018-09-19 13:17:17 +0530

    Update Constants.java

--- keycloak/sms-provider/src/main/java/org/sunbird/keycloak/utils/Constants.java
+++ keycloak/sms-provider/src/main/java/org/sunbird/keycloak/utils/Constants.java
@@ -13,17 +13,17 @@ public class Constants {
   public static final String USERNAME = "userName";
   public static final String EXPIRATION_IN_SECS = "expirationInSecs";
   public static final String IS_AUTH_REQUIRED = "isAuthRequired";
-  public static final String NOT_AUTHORIZED = "Not Authorized.";
-  public static final String DOES_NOT_HAVE_REALM_ADMIN_ROLE = "Does not have realm admin role.";
-  public static final String USER_IS_DISABLED = "User is disabled.";
-  public static final String CLIENT_ID_MISSING = "Client id missing.";
-  public static final String NOT_ENABLED = " not enabled";
   public static final String KEY = "key";
   public static final String LINK = "link";
-  public static final String FAILED_TO_CREATE_LINK = "Failed to create link";
   public static final String BEARER = "Bearer";
   public static final String ADMIN = "admin";
-  public static final String INVALID_PARAMETER_VALUE = "Invalid value {0} for parameter {1}.";
   public static final int DEFAULT_LINK_EXPIRATION_IN_SECS = 7200;
+  
+  public static final String ERROR_NOT_ENABLED = " not enabled";
+  public static final String ERROR_NOT_AUTHORIZED = "Not Authorized.";
+  public static final String ERROR_USER_IS_DISABLED = "User is disabled.";
+  public static final String ERROR_CREATE_LINK = "Failed to create link";
+  public static final String ERROR_REALM_ADMIN_ROLE_ACCESS = "Does not have realm admin role.";
+  public static final String ERROR_INVALID_PARAMETER_VALUE = "Invalid value {0} for parameter {1}.";
 
 }
